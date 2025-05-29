be82d6a60f1420797104128066a7974e7cdfc036
FamyLeVrai <109310331+FamyLeVrai@users.noreply.github.com>
2025-05-29 18:16:54 +0200

    Update Password Generator.py
    Remove useless " \n " thanks

--- Password Generator.py
+++ Password Generator.py
@@ -1,6 +1,5 @@
 import random
 import string
-import sys
 
 all_characters = list(string.ascii_letters + string.digits + string.punctuation)
 
@@ -8,26 +7,26 @@ def passwordGen():
     while True:
         try:
             print("\n" + "="*40)  # Ligne de séparation
-            Value = int(input("\nEnter a limit of characters: "))  
+            Value = int(input("Enter a limit of characters: "))  
 
             if Value <= 0:
-                print("\nMerci d'entrer un nombre positif.")
+                print("Merci d'entrer un nombre positif.")
                 continue
 
             password = "".join(random.choice(all_characters) for _ in range(Value))
-            print("\nGenerated Password: " + password + "\n")
+            print("Generated Password: " + password)
 
             print("="*40)  # Autre séparation
-            again = input("\nDo you want to generate another one? (yes/no): ").lower()
+            again = input("Do you want to generate another one? (yes/no): ").lower()
 
             if again == "no":
-                print("\nExiting password generator...\n")
-                sys.exit()
+                print("Exiting password generator...\n")
+                break
             elif again != "yes":
-                print("\nInvalid input, exiting...\n")
-                sys.exit()
+                print("Invalid input, exiting...\n")
+                break
 
         except ValueError:
-            print("\nMerci d'entrer une valeur valide.\n")
+            print("Merci d'entrer une valeur valide.\n")
 
 passwordGen()
